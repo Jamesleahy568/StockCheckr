f44efcdd90f5690e1b2961592c48eb77494fbc77
leahyj4 <james.leahy4@mail.dcu.ie>
2019-03-20 13:59:43 +0000

    Initial Layout Code

---
+++ app/src/main/java/com/example/stockcheckr/NewlookBranches.java
@@ -0,0 +1,13 @@
+package com.example.stockcheckr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewlookBranches extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newlook_branches);
+    }
+}
---
+++ app/src/main/java/com/example/stockcheckr/ProductCode.java
@@ -0,0 +1,13 @@
+package com.example.stockcheckr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_code);
+    }
+}
